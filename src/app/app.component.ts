af6b2fd7b43e56eef4506fa5a981ae99362a67ac
RABHI-Med-Amine <mohammedamine.rabhi@sii-maroc.com>
2022-04-08 15:56:07 +0000

    implementing the basic app function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SenetencesJSON from '../assets/text.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typing-game';
+
+export class AppComponent implements OnInit{
+  sentences = SenetencesJSON;
+  randomText!: string;
+  solved = false;
+  entredText = '';
+
+  ngOnInit(): void {
+    this.randomText = this.sentences[Math.floor(Math.random() * this.sentences.length)].sentence; 
+
+  }
+
+  onInput(value:string){
+    this.entredText = value;
+    if(this.randomText === value){
+      this.solved = true;
+    }else{
+      this.solved = false;
+    }
+  }
+  
+compare(entred:string,random:string){
+  if(!entred)
+    return 'panding'
+  
+  return entred === random ? 'correct' : 'incorrect';   
+}
+
+
+
 }
